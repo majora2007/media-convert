8a44db260f718327038a246c53afd93a97739ac5
Joseph Milazzo <josephmajora@gmail.com>
2016-01-20 20:30:45 -0600

    Fix some unicode issues, pulled progress bar code into another file, refactored process functions

--- media_convert.py
+++ media_convert.py
@@ -7,39 +7,12 @@ from pymediainfo import MediaInfo
 import subprocess
 import psutil
 import time
+from progressbar import (update_progress, restart_line)
 
 
 #######################################################################
 #                       PROGRESS BAR
-import time, sys
-def restart_line():
-    sys.stdout.write('\r')
-    sys.stdout.flush()
 
-def update_progress(progress):
-    '''
-        update_progress() : Displays or updates a console progress bar
-        Accepts a float between 0 and 1. Any int will be converted to a float.
-        A value under 0 represents a 'halt'.
-        A value at 1 or bigger represents 100%
-    '''
-    barLength = 10 # Modify this to change the length of the progress bar
-    status = ""
-    if isinstance(progress, int):
-        progress = float(progress)
-    if not isinstance(progress, float):
-        progress = 0
-        status = "error: progress var must be float\r\n"
-    if progress < 0:
-        progress = 0
-        status = "Halt...\r\n"
-    if progress >= 1:
-        progress = 1
-        status = "Done...\r\n"
-    block = int(round(barLength*progress))
-    text = "\rPercent: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
 #######################################################################
 
 def setup_logger(dir, filename, debug_lvl):
@@ -57,6 +30,7 @@ def setup_logger(dir, filename, debug_lvl):
 
     logging.basicConfig(filename=log_filePath,level=debug_lvl, format='%(asctime)s %(message)s')
 
+valid_extensions = ['rmvb', 'mkv', 'avi']
 def needs_convert(file):
     return file.endswith('mkv') or file.endswith('avi')
 
@@ -115,40 +89,24 @@ def delete(path):
     logger.info('Deleting ' + path)
     os.remove(path)
 
-def process_remux(remuxes):
-    i = 0
-    for i in range(0, len(remuxes)):
-        restart_line()
-        update_progress((i*1.0)/float(len(remuxes)))
-        media = remuxes[i]
-        cpu_perc = psutil.cpu_percent(interval=1)
-        if cpu_perc < MAX_CPU_THRESHOLD:
-            print 'Processing ' + media.tracks[0].complete_name
-            if remux(media.tracks[0].complete_name) is 0:
-                if DELETE:
-                    delete(media.tracks[0].complete_name)
-                print media.tracks[0].complete_name + ' Fully processed'
-            else:
-                logger.info('Failed to remux ' + media.tracks[0].complete_name)
-                print('Failed to remux ' + media.tracks[0].complete_name)
-            i = i + 1
-        else:
-            logger.info('CPU utilization is ' + str(cpu_perc) + ' which is greater than ' + str(MAX_CPU_THRESHOLD) + '. Sleeping for 15 secs')
-            print('CPU utilization is ' + str(cpu_perc) + ' which is greater than ' + str(MAX_CPU_THRESHOLD) + '. Sleeping for 15 secs')
-            time.sleep(15)
 
-def process_transcodes(handbrakes):
+def process(video_list):
     i = 0
-    while(i < len(handbrakes)):
+    while(i < len(video_list)):
         restart_line()
-        update_progress((i*1.0)/float(len(handbrakes)))
-        media = handbrakes[i]
+        update_progress((i*1.0)/float(len(video_list)))
+        media = video_list[i]
+
         cpu_perc = psutil.cpu_percent(interval=1)
         if cpu_perc < MAX_CPU_THRESHOLD:
-            print 'Processing ' + media.tracks[0].complete_name
+            print 'Processing ' + str(media.tracks[0].complete_name)
             if convert(media.tracks[0].complete_name) is 0:
                 if DELETE:
-                    delete(media.tracks[0].complete_name)
+                    try:
+                        delete(media.tracks[0].complete_name)
+                    except OSError:
+                        logger.exception('There was an issue deleting ' + str(media.tracks[0].complete_name))
+                        # Idea: Delete the byproduct instead, we can attempt delete on next run
                 print media.tracks[0].complete_name + ' Fully processed'
             else:
                 logger.info('Failed to convert ' + media.tracks[0].complete_name)
@@ -163,7 +121,7 @@ handbrake_cli = 'HandBrakeCLI -i "{{source}}" -o "{{destination}}" --preset="Ple
 ffmpeg_cli = 'ffmpeg -i "{{source}}" -vcodec copy -acodec copy "{{destination}}"'
 
 # A list of directories to scan
-watched_folders = ['K:\Series\Game of Thrones\Season 01']
+watched_folders = ['K:/Series', 'K:/Movies']
 exclude = []
 
 # Paths to all valid files
@@ -178,6 +136,12 @@ MAX_CPU_THRESHOLD = 50 #30
 
 # Flag to denote whether to delete source files after successfull encode
 DELETE = True
+
+# Flag to denote whether to just run MediaInfo on files
+JUST_CHECK = False
+
+# Memory chunks before a processing session should be kicked off (if JUST_CHECK then we just free memory)
+MEMORY_CHUNK = 10 # This is 200 MediaInfo objects
 
 # EXT should be either mp4 or m4v. m4v should be chosen if you have multi-track audio and Apple TV users
 global EXT
@@ -194,24 +158,30 @@ if __name__ == '__main__':
         base_path = normalize_path(base_path)
         print 'Searching for files in ' + base_path
         logger.info('Searching for files in ' + base_path)
+        t0 = time.time()
         for root, dirs, files in os.walk(base_path, topdown=True):
             dirs[:] = [d for d in dirs if d not in exclude]
             for file in files:
                 if needs_convert(file):
                     path = os.path.join(root, file)
                     paths.append(normalize_path(path))
+        t1 = time.time()
+        print '[Directory Scan] Execution took %s ms' % str(t1-t0)
 
 
 
     logger.info('=====Scan Complete=====')
     logger.info('Total files scanned: ' + str(len(paths)))
+    print('Total files scanned: ' + str(len(paths)))
     print('Calculating conversions...')
+    t0 = time.time()
     count = 0.0
     for path in paths:
         count += 1.0
         restart_line()
         update_progress(float(count/len(paths)))
 
+        logger.debug('Calculating MediaInfo for: ' + normalize_path(path))
         media_info = MediaInfo.parse(normalize_path(path))
         for track in media_info.tracks:
             if track.track_type == 'Video':
@@ -219,19 +189,25 @@ if __name__ == '__main__':
                     remuxes.append(media_info)
                 else:
                     handbrakes.append(media_info)
-
+    t1 = time.time()
 
     logger.info('Total Remuxes needed: ' + str(len(remuxes)))
     logger.info('Total Re-encodes needed: ' + str(len(handbrakes)))
     print('Total Remuxes needed: ' + str(len(remuxes)))
     print('Total Re-encodes needed: ' + str(len(handbrakes)))
     print '\n\n'
+    print '[Media Check] Execution took %s ms' % str(t1-t0)
+
+    if JUST_CHECK:
+        sys.exit()
 
 
     # Start batch processing Re-encodes
-    process_transcodes(handbrakes)
+    #process_transcodes(handbrakes)
+    #process(handbrakes)
 
     # Start batch processing Re-encodes
-    process_remux(remuxes)
+    #process_remux(remuxes)
+    process(remuxes)
 
 
